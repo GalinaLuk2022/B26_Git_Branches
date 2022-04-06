14b0f7c3f707875d46a48e0c934a982f2b3dfe19
GalinaLuk2022 <98920382+GalinaLuk2022@users.noreply.github.com>
2022-04-06 19:45:17 -0400

    create the loo in master

--- src/test_cases/Problem.java
+++ src/test_cases/Problem.java
@@ -1,12 +1,11 @@
 package test_cases;
 
 public class Problem {
+
     public static void main(String[] args) {
 
-        for (int i=0; i<5; i++){
-            System.out.println(i);
+        for (int j =0; j<6; j++){
+            System.out.println(j);
         }
-
     }
 }
-
